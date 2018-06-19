41a7d43c9d76421a7506f03292e4f814a34796b6
mark sauer-utley <mark.utley.713@gmail.com>
2018-06-19 17:16:29 -0400

    add symbols

--- bin/run.rb
+++ bin/run.rb
@@ -1,14 +1,4 @@
 require_relative '../config/environment'
 
-#to get chart info from api...
-#CLIENT.call(day, month, year, hour, minute, lat, long, timezone)
-
-#Models for signs, planets, users, joiner table
-
-#ways to continue prompting user to do stuff until the exit
-
 puts "~welcome~to~astroCLI~"
-
-
-
 run
