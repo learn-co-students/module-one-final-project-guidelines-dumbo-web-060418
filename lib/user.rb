5a92ff985645ed2b51f1e499cbf5199d6de28b0d
TezSmith <Montez.Smith1@gmail.com>
2018-06-19 15:01:09 -0400

    Modified User Options

--- lib/user.rb
+++ lib/user.rb
@@ -2,51 +2,57 @@ class User < ActiveRecord::Base
   has_many :user_meals
   has_many :meals, through: :user_meals
 
+  #works but is not linked to EST
   def welcome
-  	time = Time.now
-  	case time
-  	when time.hour == 0..11
-  		puts "Good morning!"
-  	when time.hour == 12..15
-  		puts "Good afternoon!"
-  	when time.hour == 16..21
-  		puts "Good night!"
-  	when time.hour == 22..24
-  		puts "NO EATING THIS LATE"
-  	end
-  end
+    	logtime = Time.now.hour
+    	case
+    	when logtime > 0 && logtime < 11
+    		puts "Good morning!"
+    	when logtime > 12 && logtime < 15
+    		puts "Good afternoon!"
+    	when logtime > 16 && logtime < 21
+    		puts "Good night!"
+    	when logtime > 22 && logtime < 23
+    		puts "NO EATING THIS LATE"
+      else
+        puts "Please try again."
+    	end
+    end
 
-  def menu_options
-  	puts "What would you like to do? Enter a number to pick your option"
-  	puts "1. Input Meal\n 
-  		  2. Check Meals\n
-  		  3. Exit"
-  		answer = gets.chomp 
-  		get_user_choice
-  		what_to_do(answer)
-  end
+   #Gets User's choice
+    def get_user_choice
+    	gets.chomp
+    end
 
-  def get_user_choice
-  	gets.chomp 
-  	end
+   #Presents User with menu options
+    def menu_options
+    	puts "What would you like to do? Enter a number to pick your option"
+    	puts "1. Input Meal\n
+    		  2. Check Meals\n
+    		  3. Exit"
+    		answer = get_user_choice
+    		what_to_do(answer)
+    end
 
-  def what_to_do
-  	if answer == 1
-			input_meal
-			puts "input meal method"
-		elsif answer == 2
-			check_meal
-			puts "check meal method"
-		elsif answer == 3 
-			#exit application
-			puts "Goodbye!"
-		else
-			puts "Not a valid choice."
-  	end
-  end
- 
+  #Performs an action based on User selection
+    def what_to_do(answer)
+    	if answer == "1"
+  			#input_meal
+  			puts "input meal method"
+  		elsif answer == "2"
+  			#check_meal
+  			puts "check meal method"
+  		elsif answer == "3"
+  			#exit application
+  			puts "Goodbye!"
+  		else
+  			puts "Not a valid choice."
+    	end
+    end
 
-  	
+
+
+
 
   # 	def input_meal
 
@@ -55,9 +61,6 @@ class User < ActiveRecord::Base
   # 	def check_meal
 
   	# end
-  
+
 
  end
-
-
-	
