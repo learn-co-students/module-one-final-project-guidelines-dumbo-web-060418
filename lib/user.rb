1def4f9fe843b2c07337897a83e50adda3bb7c1b
QMaximillian <quinnlashinsky@gmail.com>
2018-06-19 12:38:53 -0400

    Added some methods to setup CLI interaction

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,56 @@
 class User < ActiveRecord::Base
   has_many :user_meals
   has_many :meals, through: :user_meals
-end
+
+  def welcome
+  	time = Time.now
+  	case time
+  	when time.hour == 0..11
+  		puts "Good morning!
+  	when time.hour == 12..15
+  		puts "Good afternoon!"
+  	when time.hour == 16..21
+  		puts "Good night!
+  	when time.hour == 22..24
+  		puts "NO EATING THIS LATE"
+  	end
+  end
+
+  def menu_options
+  	puts "What would you like to do? Enter a number to pick your option"
+  	puts "1. Input Meal\n 
+  		  2. Check Meals\n
+  		  3. Exit"
+  		  get_user_choice
+  		if get_user_choice = 1
+  			input_meal
+  		elsif get_user_choice = 2
+  			check_meal
+  		elsif get_user_choice = 3 
+  			#exit application
+  			puts "Goodbye!"
+  		else
+  			"Not a valid choice"
+  		end
+  end
+
+  		
+  end
+
+  	def get_user_choice
+  	choice = gets.chomp 
+  	end
+
+  	def input_meal
+
+ 	end
+
+  	def check_meal
+
+  	end
+  
+
+ end
+
+
+	
