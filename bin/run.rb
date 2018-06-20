e1a8479b52521a1057b4b53ff72d956e3156e002
TezSmith <Montez.Smith1@gmail.com>
2018-06-20 17:48:01 -0400

    Completed CR of CRUD.

--- bin/run.rb
+++ bin/run.rb
@@ -1,11 +1,8 @@
 require_relative '../config/environment'
 require 'pry'
 
-# welcome
-# get_name
-# menu_options
 controller = MealController.new
 controller.welcome
 controller.menu_options
 
-binding.pry
+binding.pry 
--- lib/meal_controller.rb
+++ lib/meal_controller.rb
@@ -6,7 +6,7 @@ def get_name
 	user_name = gets.chomp
 	user = User.find_or_create_by(name: user_name)
 	self.update(user_id: user.id)
-end	
+end
 
 def welcome
     get_name
@@ -38,7 +38,9 @@ def welcome
     puts "What would you like to do? Enter a number to pick your option"
     puts "1. Input Meal"
     puts "2. Check Meals"
-    puts "3. Exit"
+    puts "3. Update Meals"
+		puts "4. Delete Meals"
+		puts "5. Exit"
       answer = get_user_choice
       what_to_do(answer)
   end
@@ -48,9 +50,13 @@ def welcome
     if answer == "1"
        input_meal
     elsif answer == "2"
-       check_meal
+       check_meals
     elsif answer == "3"
-      puts "Goodbye"
+       update_meals
+		elsif answer == "4"
+       delete_meal
+		elsif answer == "5"
+			 puts "Goodbye!"
     else
       puts "Not a valid choice. Please try again."
       return_home
@@ -61,8 +67,9 @@ def welcome
   def input_meal
      puts "Please enter the name of your meal:"
      food = gets.chomp #should make sure a string is entered and eventually be editable
-    		puts "Please enter your meal calories:"
+    		puts "Please enter your meal calories (Press 1 to return to menu):"
     		calories = gets.chomp
+
     		puts "Please enter your sugar intake:"
     		sugar = gets.chomp
     		puts "Please enter your salt intake:"
@@ -77,7 +84,6 @@ def welcome
     	 UserMeal.create(user_id: self.user_id, meal_id: meal.id)
     	 menu_options
 
-   
    end
 
 
@@ -86,19 +92,49 @@ def welcome
     Meal.create(food: food, calories: calories, sugar: sugar, salt: salt, carbs: carbs, protein: protein)
   end
 
- #Helper method for check meals
-  def get_meals
-    UserMeal.all.select do |user_meal|
-        user_meal.user == self
-      end
-  end
-
  #Gets all the meals belonging to the User
   def check_meals
-    self.get_meals.map do |user_meals|
-      user_meals.meal
-    end
+    user.meals.each do |meal|
+       puts meal
+		end
+		menu_options
   end
+
+	#Gets user's updated meal information
+	def my_meal_update_questions
+		 puts "Please enter your meal calories:"
+		 calories = gets.chomp
+		 puts "Please enter your sugar intake:"
+		 sugar = gets.chomp
+		 puts "Please enter your salt intake:"
+		 salt = gets.chomp
+		 puts "Please enter your carb intake:"
+		 carbs = gets.chomp
+		 puts "Please enter your protein intake:"
+		 protein = gets.chomp
+	end
+
+ #Updates a part or all of a user's meal instance
+	def update_meals
+		puts "Enter the name of the meal you want to update:"
+    meal_name = gets.chomp
+    my_meal = user.meals.find_by(food:meal_name)
+		my_meal_update_questions
+		Meal.update(user_id: self.user_id, calories: calories, sugar: sugar, salt: salt, carbs: carbs, protein: protein)
+    binding.pry
+	end
+
+	#Deletes a user's meal instance
+ 	def delete_meal
+ 		puts "Enter the name of the meal you want to delete:"
+     meal_name = gets.chomp
+     user.meals.select do |meal|
+        if meal.food == meal_name
+           meal.delete
+					 puts "Your meal has been deleted."
+ 			end
+ 		end
+ 	end
 
 #Returns User to home screen
   def return_home
@@ -106,15 +142,15 @@ def welcome
     menu_options
   end
 
-  #+++++++TOMORROW+++++++
+  #+++++++NEXT UP+++++++
 
-  #Delete existing database tables.
-  #Repopulate database tables.
-  #Ask question about how do we return and feed data to the database
-  #especially after a user inputs their meal information.
-  #Figure out time zone in our welcome method
-  
-	# Pry.start
+  #Give users the ability to delete their meal entry
+	#and/or all associated meals.
+	#Give users the ability to update their meal entry
+
+	# if calories == "1"
+	# 	menu_options
+	# end
 
 
 end
