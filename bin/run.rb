6dd7e9888157b0da3e60a60a8b7981e89ea22431
QMaximillian <quinnlashinsky@gmail.com>
2018-06-22 13:40:34 -0400

    Add ability to play song

--- bin/run.rb
+++ bin/run.rb
@@ -2,9 +2,13 @@ require_relative '../config/environment'
 require 'pry'
 require 'terminal-table'
 
+pid = fork{ exec 'afplay', "media/PokeThemes2.mp3"}
+
 controller = MealController.new
 controller.welcome
 controller.menu_options
+
+pid = fork{ exec 'killall', "afplay" }
 
  #Things to Add
 
